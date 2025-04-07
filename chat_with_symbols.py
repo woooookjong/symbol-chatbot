4ba273c9b1544b6a90f96f008f64f5ce4531fb25
woooookjong <kjwpcpkjpk@gmail.com>
2025-04-07 13:19:52 +0900

    Update chat_with_symbols.py

--- chat_with_symbols.py
+++ chat_with_symbols.py
@@ -30,7 +30,7 @@ if not st.session_state.authenticated:
     pw = st.text_input("비밀번호를 입력하세요", type="password")
     if pw == PASSWORD:
         st.session_state.authenticated = True
-        st.experimental_rerun()
+        st.rerun()
     else:
         st.stop()
 
